943dacb31586433a6aacb556ed0a43b06db02083
JoshuaZe <joshua_zhang@foxmail.com>
2015-09-07 23:40:09 +0800

    Traditional Network TM starting

--- restopicer-research/LinkTopicModel/code/demo/main_for_demo_TNTM.R
+++ restopicer-research/LinkTopicModel/code/demo/main_for_demo_TNTM.R
@@ -12,14 +12,14 @@ load(file = "rdata/demo.RData")
 data <- unique(demoPapersKeywords)
 bi_matrix <- table(data$item_ut,tolower(data$author_keyword))
 # bipartite network max compart
-# bi_compart <- compart(bi_matrix)
-# bi_compart$n.compart
-# size.compart <- data.frame(doc=row.names(bi_compart$cweb),compart=-apply(bi_compart$cweb,1,FUN = min)) %>% group_by(compart) %>% summarise(cnt = n())
-# max.size.compart <- size.compart[which.max(size.compart$cnt),]$compart
-# doc.compart <- data.frame(doc=row.names(bi_compart$cweb),compart=-apply(bi_compart$cweb,1,FUN = min)) %>% filter(compart == max.size.compart)
-# bi_MaxCompart <- bi_matrix[row.names(bi_matrix) %in% doc.compart$doc,]
-# bi_MaxCompart[which(bi_MaxCompart!=0)] <- 1
-# bi_MaxCompart <- empty(bi_MaxCompart)
+bi_compart <- compart(bi_matrix)
+bi_compart$n.compart
+size.compart <- data.frame(doc=row.names(bi_compart$cweb),compart=-apply(bi_compart$cweb,1,FUN = min)) %>% group_by(compart) %>% summarise(cnt = n())
+max.size.compart <- size.compart[which.max(size.compart$cnt),]$compart
+doc.compart <- data.frame(doc=row.names(bi_compart$cweb),compart=-apply(bi_compart$cweb,1,FUN = min)) %>% filter(compart == max.size.compart)
+bi_MaxCompart <- bi_matrix[row.names(bi_matrix) %in% doc.compart$doc,]
+bi_MaxCompart[which(bi_MaxCompart!=0)] <- 1
+bi_MaxCompart <- empty(bi_MaxCompart)
 # new corpus_dtm is the bi_MaxCompart
 
 
