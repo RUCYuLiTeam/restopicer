b226774338ea1b47b4e2dc3f62cbbdbaff380c47
JoshuaZe <joshua_zhang@foxmail.com>
2015-09-17 09:38:45 +0800

    add important method for evaluation community and topic

--- restopicer-research/NetworkBasedTopicModel/code/functions_for_community_evaluation.R
+++ restopicer-research/NetworkBasedTopicModel/code/functions_for_community_evaluation.R
@@ -1,5 +1,8 @@
 ######
 # community evaluation functions and plot report
+# Community Detection Result comparison
+# Testing the significance of a community
+# for different perspect
 ######
 # comunity.coverage<-calcommunity.coverage(topic_term)
 # [0,1], the larger the better
@@ -53,4 +56,83 @@ caloverlap.quality<- function(comm_member,df_bi_data,df_doc_tag){
   v.real <- v.real[order(names(v.real),decreasing = F)]
   # cal the mutual information
   entropy::mi.plugin(matrix(c(v.model,v.real),nrow = 2,ncol = length(v.real)),unit = "log2")
+}
+community.significance.test <- function(graph, vs, ...) {
+  if (is.directed(graph)) stop("This method requires an undirected graph")
+  subgraph <- induced.subgraph(graph, vs)
+  in.degrees <- degree(subgraph)
+  out.degrees <- degree(graph, vs) - in.degrees
+  wilcox.test(in.degrees, out.degrees, ...)
+}
+# doc-topics-sc
+doc.tagging.test <- function(taggingtest_data,filename,path = "output/", LeaveOneOut = FALSE){
+  # folder
+  if(!file.exists(path)) dir.create(path,recursive = TRUE)
+  # start
+  taggingtype <- unique(taggingtest_data[,dim(taggingtest_data)[2]])
+  taggingtest_result <- data.frame()
+  for(type in taggingtype){
+    data <- taggingtest_data[,2:dim(taggingtest_data)[2]]
+    data[,dim(data)[2]] <- (taggingtest_data[,dim(taggingtest_data)[2]]==type)
+    colnames(data)[dim(data)[2]] <- "binary_class"
+    result <- data.frame()
+    if(LeaveOneOut){
+      for(i in 1:dim(data)[1]){
+        train.data <- data[-i,]
+        test.data <- data[i,]
+        fit <- glm(binary_class ~.,family=binomial(link='logit'),data=train.data)
+        test.result <- predict.glm(fit,test.data[,1:(dim(data)[2]-1)],type = "response")
+        result0 <- data.frame(real.y=test.data$binary_class,model.y=(test.result>=0.5),fitted.values=test.result)
+        result <- rbind(result,result0)
+      }
+    }else{
+      fit <- glm(binary_class ~.,family=binomial(link='logit'),data=data)
+      summary(fit)
+      R2_Cox.Snell <- 1-exp((fit$deviance-fit$null.deviance)/nrow(fit$data))#计算Cox-Snell拟合优度
+      R2_Nagelkerke <- R2_Cox.Snell/(1-exp((-fit$null.deviance)/nrow(fit$data)))#计算Nagelkerke拟合优度，我们在最后输出这个拟合优度值
+      result <- data.frame(real.y=(fit$y==1),model.y=(fit$fitted.values>=0.5),fitted.values=fit$fitted.values)
+    }
+    #caret::confusionMatrix
+    result.table <- table(factor(result$model.y,levels=c("TRUE","FALSE")),factor(result$real.y,levels=c("TRUE","FALSE")))
+    #result.prediction <- prediction(result$fitted.values,result$model.y)
+    # for positive class
+    precision <- result.table["TRUE","TRUE"]/sum(result.table["TRUE",])
+    sensitivity <- recall <- result.table["TRUE","TRUE"]/sum(result.table[,"TRUE"])
+    miss_rate <- 1 - recall
+    specificity <- result.table["FALSE","FALSE"]/sum(result.table[,"FALSE"])
+    distribution <- prevalence <- sum(result.table[,"TRUE"])/sum(result.table)
+    PPV <- (sensitivity * prevalence)/((sensitivity*prevalence) + ((1-specificity)*(1-prevalence)))
+    NPV <- (specificity * (1-prevalence))/(((1-sensitivity)*prevalence) + ((specificity)*(1-prevalence)))
+    detection_prevalence <- sum(result.table["TRUE",])/sum(result.table)
+    detection_rate <- detection_prevalence * precision
+    #detection_rate <- prevalence * sensitivity
+    f_measure <- 2*precision*recall/(precision+recall)
+    # for over all
+    accuracy <- distribution*recall+(1-distribution)*specificity
+    error_rate <- 1 - accuracy
+    balanced_accuracy <- (sensitivity+specificity)/2
+    Mcnemar_Test.PValue <- mcnemar.test(result.table)$p.value
+    no_information_rate <- max(distribution,1-distribution)
+    # roc and auc
+    if(length(unique(result$fitted.values))>5){
+      ROC_plot <- roc(result$real.y, result$fitted.values,smooth=T,percent=T)
+    }else{
+      ROC_plot <- roc(result$real.y, result$fitted.values,percent=T)
+    }
+    # plot ROC
+    png(file.path(path,paste(filename,type,"ROC.png",sep="-")),width=500,height=500)
+    par(mar=c(5,4,4,2))
+    plot(ROC_plot,max.auc.polygon=T,auc.polygon=T,grid=T,show.thres=T,print.auc=T,main=type,cex.main=1)
+    dev.off()
+    #auc <- mean(sample(pos.decision,1000,replace=T) > sample(neg.decision,1000,replace=T))
+    AUC <- ROC_plot$auc
+    result.measure <- data.frame(filename,tagging.type=type,
+                                 accuracy,error_rate,balanced_accuracy,
+                                 Mcnemar_Test.PValue,AUC,no_information_rate,
+                                 sensitivity,recall,miss_rate,specificity,
+                                 distribution,prevalence,detection_prevalence,PPV,NPV,
+                                 precision,detection_rate,f_measure)
+    taggingtest_result <- rbind(taggingtest_result,result.measure)  
+  }
+  write.table(taggingtest_result,file = file.path(path,paste(filename,"taggingtest.txt",sep="-")),quote = F,sep = "\t",row.names = F,col.names = T)
 }
