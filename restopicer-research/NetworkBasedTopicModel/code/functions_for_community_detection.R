6594a963f9aca8a2000d31ee41f6334f8572f5ca
JoshuaZe <joshua_zhang@foxmail.com>
2015-09-17 09:30:31 +0800

    functions for community detection

---
+++ restopicer-research/NetworkBasedTopicModel/code/functions_for_community_detection.R
@@ -0,0 +1,58 @@
+######
+# Community Detection In R
+# igraph
+# http://igraph.wikidot.com/community-detection-in-r
+######
+# g <- sample_gnp(100, 0.3)
+# k <- 6
+clique.community <- function(graph, k, threshold = 1) {
+  clq <- cliques(graph, min=k, max=k)
+  edges <- c()
+  for (i in seq_along(clq)) {
+    for (j in (i+1):length(clq)) {
+      # (K-threshold)/(K+threshold)
+      if ( length(unique(c(clq[[i]], clq[[j]]))) <= k+threshold ) {
+        edges <- c(edges, c(i,j)-1)
+      }
+    }
+  }
+  clq.graph <- simplify(graph(edges))
+  V(clq.graph)$name <- seq_len(vcount(clq.graph))
+  comps <- decompose.graph(clq.graph)
+  
+  lapply(comps, function(x) {
+    unique(unlist(clq[ V(x)$name ]))
+  })
+}
+largeScaleCommunity <- function(g,mode="all"){
+  V(g)$group <- as.character(V(g))
+  thisOrder <- sample(vcount(g),vcount(g))-1
+  t <- 0
+  done <- FALSE
+  while(!done){
+    t <- t+1
+    cat("\rtick:",t)
+    done <- TRUE ## change to FALSE whenever a node changes groups              
+    for(i in thisOrder){
+      ## get the neighbor group frequencies:                                    
+      groupFreq <- table(V(g)[neighbors(g,i,mode=mode)]$group)
+      ## pick one of the most frequent:                                         
+      newGroup <- sample(names(groupFreq) [groupFreq==max(groupFreq)],1)
+      if(done){done <- newGroup==V(g)[i]$group}
+      V(g)[i]$group <- newGroup
+    }
+  }
+  ## now fix any distinct groups with same labels:                              
+  for(i in unique(V(g)$group)){
+    ## only bother for connected groups                                         
+    if(!is.connected(subgraph(g,V(g)[group==i]))){
+      theseNodes <- V(g)[group==i]
+      theseClusters <- clusters(subgraph(g,theseNodes))
+      ## iterate through the clusters and append their names                    
+      for(j in unique(theseClusters$membership)){
+        V(g)[theseNodes[theseClusters$membership==j]]$group <- paste(i,j,sep=".")
+      }
+    }
+  }
+  return(g)
+}
