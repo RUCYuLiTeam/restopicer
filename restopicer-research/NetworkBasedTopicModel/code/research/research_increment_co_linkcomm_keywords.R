2e3a1400a42b1880988bd3c78d75b8d19e6de2e0
JoshuaZe <joshua_zhang@foxmail.com>
2015-10-04 20:11:03 +0800

    bug fix

--- restopicer-research/NetworkBasedTopicModel/code/research/research_increment_co_linkcomm_keywords.R
+++ restopicer-research/NetworkBasedTopicModel/code/research/research_increment_co_linkcomm_keywords.R
@@ -34,13 +34,14 @@ addPersistentObjects("foldername")
 # result_linkcomm.percolation_evcent_cos <- topicDiscovery.linkcomm.percolation(data = researchPapersKeywords,datatype = "keywords",MST_Threshold = 0,percolation_threshold=th,cutat = NULL,topic_term_weight = "evcent",doc_topic_method = "similarity.cos",plotPath,plotReport = F,papers_tags_df = researchPapersSubjectCategory,link_similarity_method="original")
 # save(result_linkcomm.percolation_evcent_cos,file = paste("rdata/research_20Ys/result_",year,".RData",sep=""))
 
-corpus_dtm <- preprocess.keywords.corpus(researchPapersKeywords)
-bi_graph <- graph_from_incidence_matrix(corpus_dtm)
-proj_graph <- bipartite_projection(bi_graph, types = NULL, multiplicity = TRUE,probe1 = NULL, which = "true", remove.type = TRUE)
-coterm_graph <- simplify(proj_graph)
+# corpus_dtm <- preprocess.keywords.corpus(researchPapersKeywords)
+# bi_graph <- graph_from_incidence_matrix(corpus_dtm)
+# proj_graph <- bipartite_projection(bi_graph, types = NULL, multiplicity = TRUE,probe1 = NULL, which = "true", remove.type = TRUE)
+# coterm_graph <- simplify(proj_graph)
 load(paste("rdata/research_20Ys/result_",2013,".RData",sep=""))
 base_community_member_list <- result_linkcomm.percolation_evcent_cos$community_member_list
 for(year in 2012:1994){
+  print(year+1)
   load(paste("rdata/research_20Ys/result_",year,".RData",sep=""))
   new_community_member_list <- result_linkcomm.percolation_evcent_cos$community_member_list
   len_base <- length(base_community_member_list)
@@ -81,13 +82,14 @@ for(year in 2012:1994){
     for(j in 1:length(left_comm_j)){
       comm_pair_df <- data.frame()
       comm_j <- left_comm_j[[j]]
-      g_j <- delete.vertices(coterm_graph,v = comm_j)
-      k_core_j <- coreness(g_j)
+      #g_j <- delete.vertices(coterm_graph,v = comm_j)
+      #k_core_j <- coreness(g_j)
       for(i in 1:length(community_member_list)){
         comm_i <- community_member_list[[i]]
-        g_i <- delete.vertices(coterm_graph,v = comm_i)
-        k_core_i <- coreness(g_i)
-        dist=mean(distances(coterm_graph,names(k_core_j)[which.max(k_core_j)],names(k_core_i)[which.max(k_core_i)],weights = 1/E(coterm_graph)$weight))/abs(length(comm_j)-length(comm_i))
+        #g_i <- delete.vertices(coterm_graph,v = comm_i)
+        #k_core_i <- coreness(g_i)
+        #dist=mean(distances(coterm_graph,names(k_core_j)[which.max(k_core_j)],names(k_core_i)[which.max(k_core_i)],weights = 1/E(coterm_graph)$weight))/abs(length(comm_j)-length(comm_i))
+        dist=length(comm_i)
         comm_pair_df <- rbind(comm_pair_df,data.frame(i,j,dist))
       }
       i <- comm_pair_df[which.min(comm_pair_df$dist)[1],"i"]
@@ -96,7 +98,7 @@ for(year in 2012:1994){
   }
   base_community_member_list <- community_member_list
 }
-community_member_list
-unlist(lapply(base_community_member_list,FUN = length))
-unlist(lapply(community_member_list,FUN = length))
-summary(unlist(lapply(community_member_list,FUN = length)))
+save(community_member_list,file = "rdata/research_20Ys/community_member_list_20Ys.RData")
+# unlist(lapply(base_community_member_list,FUN = length))
+# unlist(lapply(community_member_list,FUN = length))
+# summary(unlist(lapply(community_member_list,FUN = length)))
