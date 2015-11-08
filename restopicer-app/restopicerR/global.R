a460ec28b34d893f1dc943d7d6609b7b5a21e31f
JoshuaZe <joshua_zhang@foxmail.com>
2015-11-08 23:22:22 +0800

    new parameters

--- restopicer-app/restopicerR/global.R
+++ restopicer-app/restopicerR/global.R
@@ -3,10 +3,24 @@ rm(list = ls(envir = globalenv()))
 library(shiny)
 library(shinythemes)
 library(shinydashboard)
+library(RMySQL)
+library(RJSONIO)
+library(RCurl)
+library(dplyr)
 #sourcing code
 source("functions.R")
 
-#initial parameter
+# elastic search config
+elastic_search_server <- "http://127.0.0.1:9200"
+indexname <- "restopicer"
+typename  <- "paper"
+es_location <- paste(elastic_search_server,indexname,typename,sep = "/")
+searchLocation <- paste(es_location,"_search",sep = "/")
+# database config
 
-#initial value
-taskprogress <- -1
+#initial parameter
+relevent_N <- 5
+preference_w <- 0.5
+quality_w <- 0.5
+composite_N <- 5
+explore_N <- 0
