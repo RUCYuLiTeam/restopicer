6f9607419d1a37993f1ccc27587e7f787777baab
JoshuaZe <joshua_zhang@foxmail.com>
2015-11-08 23:22:09 +0800

    ui and basic server

--- restopicer-app/restopicerR/server.R
+++ restopicer-app/restopicerR/server.R
@@ -1,12 +1,25 @@
 function(input, output, session) {
+  mission_info <- data.frame()
+  recommend_paper <- data.frame()
   # taskMenu dropdownMenuOutput for showing taskprogress
-  output$taskMenu <- renderMenu({
-    dropdownMenu(taskItem("Task Progress", value = taskprogress, color = "aqua", href = NULL),type = "tasks",badgeStatus = NULL)
+  #output$taskMenu <- renderMenu({
+  #  input$loginmission
+  #  dropdownMenu(taskItem("Task Progress", value = taskprogress, color = "aqua", href = NULL),type = "tasks",badgeStatus = NULL)
+  #})
+  # text for missionid
+  output$missionid <- renderText({
+    input$loginmission
+    input$recommend
+    if(nrow(mission_info)==1){
+      paste("Your Mission Id: ",mission_info$mission_id," (Round ",mission_info$round,")",sep = "")
+    }else{
+      "Sorry, No Mission Found!"
+    }
   })
   # sidebarMenuOutput
   output$menu <- renderMenu({
-    goRecommend()
-    if(taskprogress>0){
+    input$loginmission
+    if(nrow(mission_info)==1){
       sidebarMenu(
         menuItem("Scientific Papers", tabName = "papers", icon = icon("files-o")),
         menuItem("Preference Dashboard", tabName = "dashboard", icon = icon("dashboard"))
@@ -14,15 +27,104 @@ function(input, output, session) {
       )
     }else{
       sidebarMenu(
-        menuItem("Questionaire", icon = icon("file-text-o"), tabName = "questionaire", badgeLabel = "Initial", badgeColor = "green")
-        #menuItem("About Restopicer", tabName = "about", icon = icon("at"))
+        menuItem("New Mission", icon = icon("file-text-o"), tabName = "questionaire", badgeLabel = "Here!", badgeColor = "green"),
+        menuItem("Mission Continues", tabName = "login", icon = icon("bolt"))
       )
     }
   })
-  # submit questionaire, start recommend, next recommend
-  goRecommend<- function(){
-    input$recommend
-    taskprogress <<- taskprogress + 1
-  }
   
+  # show recommended papers
+  output$title1 <- renderText({
+    input$recommend
+    recommend_paper$article_title[1]
+  })
+  output$title2 <- renderText({
+    input$recommend
+    recommend_paper$article_title[2]
+  })
+  output$title3 <- renderText({
+    input$recommend
+    recommend_paper$article_title[3]
+  })
+  output$title4 <- renderText({
+    input$recommend
+    recommend_paper$article_title[4]
+  })
+  output$title5 <- renderText({
+    input$recommend
+    recommend_paper$article_title[5]
+  })
+  output$abstract1 <- renderText({
+    input$recommend
+    recommend_paper$abstract[1]
+  })
+  output$abstract2 <- renderText({
+    input$recommend
+    recommend_paper$abstract[2]
+  })
+  output$abstract3 <- renderText({
+    input$recommend
+    recommend_paper$abstract[3]
+  })
+  output$abstract4 <- renderText({
+    input$recommend
+    recommend_paper$abstract[4]
+  })
+  output$abstract5 <- renderText({
+    input$recommend
+    recommend_paper$abstract[4]
+  })
+  output$detail1 <- renderTable({
+    input$recommend
+    rownames(recommend_paper) <- NULL
+    recommend_paper[1,c(4,5,6,7)]
+  })
+  output$detail2 <- renderTable({
+    input$recommend
+    rownames(recommend_paper) <- NULL
+    recommend_paper[2,c(4,5,6,7)]
+  })
+  output$detail3 <- renderTable({
+    input$recommend
+    rownames(recommend_paper) <- NULL
+    recommend_paper[3,c(4,5,6,7)]
+  })
+  output$detail4 <- renderTable({
+    input$recommend
+    rownames(recommend_paper) <- NULL
+    recommend_paper[4,c(4,5,6,7)]
+  })
+  output$detail5 <- renderTable({
+    input$recommend
+    rownames(recommend_paper) <- NULL
+    recommend_paper[5,c(4,5,6,7)]
+  })
+  # login mission, mission continues
+  observe({
+    input$loginmission
+    mission_info <<- getMissionInfo(isolate(input$loginmissionid))
+    if(nrow(mission_info)==1){
+      recommend_paper <<- goRecommendation(mission_info$mission_id,mission_info$round) 
+    }
+  })
+  # add searching keywords
+  observe({
+    input$addkeyword
+    if(nrow(mission_info)==1){
+      addPreferenceKeyword(mission_info$mission_id,isolate(input$searchkeyword))
+    }
+  })
+  # go recommend
+  observe({
+    input$recommend
+    if(nrow(mission_info)==1&&nrow(recommend_paper)==5){
+      rating(mission_info$mission_id,recommend_paper$item_ut[1],input$rate1)
+      rating(mission_info$mission_id,recommend_paper$item_ut[2],input$rate2)
+      rating(mission_info$mission_id,recommend_paper$item_ut[3],input$rate3)
+      rating(mission_info$mission_id,recommend_paper$item_ut[4],input$rate4)
+      rating(mission_info$mission_id,recommend_paper$item_ut[5],input$rate5)
+      recommend_paper <<- goRecommendation(mission_info$mission_id,mission_info$round)
+      mission_info <<- getMissionInfo(mission_info$mission_id)
+    }
+  })
 }
--- restopicer-app/restopicerR/ui.R
+++ restopicer-app/restopicerR/ui.R
@@ -1,14 +1,17 @@
 dashboardPage(
-  dashboardHeader(dropdownMenuOutput("taskMenu"),title = strong("Restopicer")),
+  dashboardHeader(#dropdownMenuOutput("taskMenu"),
+                  title = strong("Restopicer")),
   dashboardSidebar(
+    strong(textOutput("missionid",inline = T)),
     sidebarMenuOutput("menu"),
     #dateRangeInput("daterange", "Date Range:",
     #               start  = "2001-01-01",end="2010-01-01",
     #               min    = "2001-01-01",max="2012-01-01",
     #               format = "yyyy",startview="decade"),
     plotOutput("wordcloud",height = "200px"),
-    sidebarSearchForm(textId="searchTerm", buttonId="recommend", label = "Search Your Keyword ..."),
-    actionButton("recommend", "Go Recommendation!",icon("thumbs-up"),width="100%")
+    sidebarSearchForm(textId="searchkeyword", buttonId="addkeyword", label = "Search Your Keyword ..."),
+    actionButton("recommend", "Go Recommendation!",icon("thumbs-up"),width="100%"),
+    actionButton("finished", "Mission Clear!",icon("times-circle"),width="100%")
   ),
   dashboardBody(
     tabItems(
@@ -16,9 +19,57 @@ dashboardPage(
       tabItem(tabName = "questionaire",
               h2("questionaire")
       ),
+      tabItem(tabName = "login",
+              textInput("loginmissionid", "Input Your Mission ID:", "1",width="300px"),
+              actionButton("loginmission", "Mission Continues!",icon("bolt"),width="300px")
+      ),
       # papers
       tabItem(tabName = "papers",
-              h2("papers tab content")
+              fluidRow(
+                tabBox(
+                  title = textOutput("title1"),
+                  # The id lets us use input$tabset1 on the server to find the current tab
+                  id = "tabset1",width = 12, 
+                  tabPanel("Detail", textOutput("abstract1"),tableOutput("detail1"),
+                           radioButtons("rate1", "Your Rating:",c("Not Interest" = "1","Hardly Interest" = "2","Somewhat Interest" = "3","Very Interest" = "4","Extremely Interest" = "5"),inline = T))
+                )
+              ),
+              fluidRow(
+                tabBox(
+                  title = textOutput("title2"),
+                  # The id lets us use input$tabset1 on the server to find the current tab
+                  id = "tabset2",width = 12, 
+                  tabPanel("Detail", textOutput("abstract2"),tableOutput("detail2"),
+                           radioButtons("rate2", "Your Rating:",c("Not Interest" = "1","Hardly Interest" = "2","Somewhat Interest" = "3","Very Interest" = "4","Extremely Interest" = "5"),inline = T))
+                )
+              ),
+              fluidRow(
+                tabBox(
+                  title = textOutput("title3"),
+                  # The id lets us use input$tabset1 on the server to find the current tab
+                  id = "tabset3",width = 12, 
+                  tabPanel("Detail", textOutput("abstract3"),tableOutput("detail3"),
+                           radioButtons("rate3", "Your Rating:",c("Not Interest" = "1","Hardly Interest" = "2","Somewhat Interest" = "3","Very Interest" = "4","Extremely Interest" = "5"),inline = T))
+                )
+              ),
+              fluidRow(
+                tabBox(
+                  title = textOutput("title4"),
+                  # The id lets us use input$tabset1 on the server to find the current tab
+                  id = "tabset4",width = 12, 
+                  tabPanel("Detail", textOutput("abstract4"),tableOutput("detail4"),
+                           radioButtons("rate4", "Your Rating:",c("Not Interest" = "1","Hardly Interest" = "2","Somewhat Interest" = "3","Very Interest" = "4","Extremely Interest" = "5"),inline = T))
+                )
+              ),
+              fluidRow(
+                tabBox(
+                  title = textOutput("title5"),
+                  # The id lets us use input$tabset1 on the server to find the current tab
+                  id = "tabset5",width = 12, 
+                  tabPanel("Detail", textOutput("abstract5"),tableOutput("detail5"),
+                           radioButtons("rate5", "Your Rating:",c("Not Interest" = "1","Hardly Interest" = "2","Somewhat Interest" = "3","Very Interest" = "4","Extremely Interest" = "5"),inline = T))
+                )
+              )
       ),
       # dashboard
       tabItem(tabName = "dashboard",
