eb075be01da55afc94a2849896498c93c00823d9
JoshuaZe <joshua_zhang@foxmail.com>
2015-11-15 21:58:11 +0800

    for analysis

--- restopicer-research/NetworkBasedTopicModel/code/research/analysis_main.R
+++ restopicer-research/NetworkBasedTopicModel/code/research/analysis_main.R
@@ -8,12 +8,34 @@
 load(file = "rdata/research_2011_2013/result_linkcomm_th=0.15_percolation.RData")
 length(result_linkcomm.percolation_evcent_cos$community_member_list)
 # analysis
-x <- result_linkcomm.percolation_evcent_cos$community_member_list[lapply(result_linkcomm.percolation_evcent_cos$community_member_list, function(x){length(x)})>20]
-length(x)
-x <- result_linkcomm.percolation_evcent_cos$topic_term[lapply(result_linkcomm.percolation_evcent_cos$community_member_list, function(x){length(x)})>15,]
+# x <- result_linkcomm.percolation_evcent_cos$community_member_list[lapply(result_linkcomm.percolation_evcent_cos$community_member_list, function(x){length(x)})>20]
+# length(x)
+# x <- result_linkcomm.percolation_evcent_cos$topic_term[lapply(result_linkcomm.percolation_evcent_cos$community_member_list, function(x){length(x)})>15,]
 
 filename=result_linkcomm.percolation_evcent_cos$model$parameter
 topic_term=result_linkcomm.percolation_evcent_cos$topic_term[lapply(result_linkcomm.percolation_evcent_cos$community_member_list, function(x){length(x)})>20,]
 topic_term=topic_term[c(3,5,6,7,8,9,10,11,13,14),]
 path="output/analysis"
 plotBipartiteMatrixReport(filename = filename,bi_matrix = topic_term,path = paste(path,"topic_term",sep = "/"),showNamesInPlot = FALSE, weightType = "tfidf", plotRowWordCloud = TRUE, plotWordCloud = F, plotRowComparison = TRUE, plotRowDist = TRUE, plotModules = FALSE)
+
+# for paper
+filename=result_linkcomm.percolation_evcent_cos$model$parameter
+mytopic<- which(lapply(result_linkcomm.percolation_evcent_cos$community_member_list, function(x){length(x)})>20)[c(3,5,6,7,8,9,10,11,13,14)]
+doc_topic=result_linkcomm.percolation_evcent_cos$doc_topic[,mytopic]
+doc_topic=doc_topic[rowSums(doc_topic)!=0,]
+apply(doc_topic, 1, function(x){which.max(x)})
+plotBipartiteMatrixReport(filename = filename,bi_matrix = doc_topic,path = paste(path,"doc_topic",sep = "/"),showNamesInPlot = FALSE, weightType = "tf", plotRowWordCloud = F, plotWordCloud = F, plotRowComparison = F, plotRowDist = TRUE, plotModules = FALSE)
+
+topic=10
+id_list=which(apply(doc_topic, 1, function(x){which.max(x)})==topic)
+doc_topic[id_list,topic][which.max(doc_topic[id_list,topic])]
+id_list[which.max(doc_topic[id_list,topic])]
+
+ids <- c(1109,1105,487,580,414,981,1023,834,602,140)
+ids <- c(1109,1105,487,580,414,674,1023,834,602,140)
+ids <- c(1109,1105,487,580,414,674,1023,897,602,140)
+dts=doc_topic[ids,]
+dts=doc_topic[c(414,1023,1109,1105,487,580,674,897,602,id_list),]
+plotBipartiteMatrixReport(filename = filename,bi_matrix = dts,path = paste(path,"doc_topic",sep = "/"),showNamesInPlot = FALSE, weightType = "tf", plotRowWordCloud = F, plotWordCloud = F, plotRowComparison = F, plotRowDist = TRUE, plotModules = FALSE)
+
+# web of science category
