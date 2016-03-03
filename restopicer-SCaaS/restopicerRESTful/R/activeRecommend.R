dad2f81b60878d038955940e9bdc626fc9c9d745
JoshuaZe <joshua_zhang@foxmail.com>
2016-03-03 22:53:34 +0800

    new data config

--- restopicer-SCaaS/restopicerRESTful/R/activeRecommend.R
+++
@@ -1,8 +0,0 @@
-activeRecommend <- function(result_relevent,rated_papers,
-                            topics_filepath,
-                            preference_w,quality_w,fresh_w,explore_w,
-                            composite_N,...){
-  result_relevent
-  
-  result_relevent[1:min(length(result_relevent),composite_N)]
-}
