3db45b1e6c20dd17fe972a06c93151b80373ac0e
JoshuaZe <joshua_zhang@foxmail.com>
2016-03-22 21:10:00 +0800

    new data for now

--- restopicer-SCaaS/restopicerRESTful/R/FreshEvaluator.R
+++ restopicer-SCaaS/restopicerRESTful/R/FreshEvaluator.R
@@ -1,3 +1,3 @@
-yearDiffReciprocalFreshEval <- function(publication_year,max_year=2013){
+yearDiffReciprocalFreshEval <- function(publication_year,max_year=2014){
   1/(max_year - as.integer(publication_year)+1)
 }
