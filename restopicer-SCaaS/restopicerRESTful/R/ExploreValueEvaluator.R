0187ba80111af8308736ff03700e4b17f8c8ed99
JoshuaZe <joshua_zhang@foxmail.com>
2016-04-17 22:09:31 +0800

    new train elastic and prediction for explore and rating predict

--- restopicer-SCaaS/restopicerRESTful/R/ExploreValueEvaluator.R
+++ restopicer-SCaaS/restopicerRESTful/R/ExploreValueEvaluator.R
@@ -3,11 +3,17 @@
 # many estimates to change will result in the improvement of many estimates,
 # and is considered useful
 activeExploreEval <- function(enetmodel,#learn a preference approximation function based on the current training set
-                              new_doc_i,test_docs,train_docs,train_rating){
+                              new_doc_i,test_docs,train_docs,train_rating,sample_size=50){
+  if(sample_size<length(train_rating)){
+    train_sample <- sample(1:length(train_rating),size = sample_size,replace = F)
+    train_docs <- train_docs[train_sample,]
+    train_rating <- train_rating[train_sample]
+  }
   ### extract the coefficient vector with L1 norm fraction=0.5
   #coef <- predict.enet(enetmodel, s=0.5, type="coef", mode="fraction")
   #sum(coef$coefficients!=0)
-  test_fits <- predict.enet(object = enetmodel, newx = test_docs, s = 0.5, type = "fit",mode = "fraction")
+  #test_fits <- predict.enet(object = enetmodel, newx = test_docs, s = 0.5, type = "fit",mode = "fraction")
+  test_fits <- predict(object = enetmodel,newx = test_docs,s=0.5,type = "link",exact = T)
   # generate new training set : add a hypothetical training point
   new_train_docs <- rbind(train_docs,test_docs[new_doc_i,])
   # final G_change
@@ -15,12 +21,15 @@ activeExploreEval <- function(enetmodel,#learn a preference approximation functi
   # for each possible rating from 1 to 5
   for(new_rating in 1:5){
     # learn a new preference approximation function based on the new training set
-    new_enetmodel <- enet(x = I(new_train_docs),y = c(train_rating,new_rating),lambda=0.5,normalize = F,intercept = T)
+    #new_enetmodel <- enet(x = I(new_train_docs),y = c(train_rating,new_rating),lambda=0.5,normalize = F,intercept = F)
+    new_enetmodel <- glmnet(x = I(new_train_docs),y = c(train_rating,new_rating),alpha=0.5,intercept = F)
     #plot(new_enetmodel)
     # for each unrated docs
-    new_test_fits <- predict.enet(object = new_enetmodel, newx = test_docs, s = 0.5, type = "fit",mode = "fraction")
+    #new_test_fits <- predict(object = new_enetmodel, newx = test_docs, s = 0.5, type = "fit",mode = "fraction")
+    new_test_fits <- predict(object = new_enetmodel, newx = test_docs, s = 0.5, type = "link",exact = T)
     # record and cal the differences between ratings estimates
-    G_change <- G_change + sum((test_fits$fit-new_test_fits$fit)^2)
+    #G_change <- G_change + sum((test_fits$fit-new_test_fits$fit)^2)
+    G_change <- G_change + sum((test_fits-new_test_fits)^2)
   }
   G_change/5
 }
--- restopicer-SCaaS/restopicerRESTful/R/PreferenceEvaluator.R
+++ restopicer-SCaaS/restopicerRESTful/R/PreferenceEvaluator.R
@@ -1,5 +1,7 @@
 elasticNetPreferenceEval <- function(enetmodel,predict_new_docs){
   #fits <- predict.enet(object = enetmodel, newx = predict_new_doc, s = dim(enetmodel$beta.pure)[1], type = "fit",mode = "step")
-  fits <- predict.enet(object = enetmodel, newx = predict_new_docs, s = 0.5, type = "fit",mode = "fraction")
-  fits$fit
+  #fits <- predict(object = enetmodel, newx = predict_new_docs, s = 0.5, type = "fit",mode = "fraction")
+  #fits$fit
+  fits <- predict(object = enetmodel,newx = predict_new_docs,s=0.5,type = "link",exact = T)
+  fits
 }
