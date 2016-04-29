5f00e13d9e9d53709e9eb152ef55f905f013f568
Joshua ZHANG <JoshuaZe@users.noreply.github.com>
2016-04-29 10:58:00 +0800

    controllername for different experiment
    controllername = "simpleHybridWeightControl"

--- restopicer-SCaaS/restopicerRESTful/R/restopicerRESTful.R
+++ restopicer-SCaaS/restopicerRESTful/R/restopicerRESTful.R
@@ -266,7 +266,7 @@ getTopKeywords <- function(username,show_k=10){
   }))
 }
 ##### goRecommendation for current mission #####
-goRecommendation <- function(username,relevent_N=50,recommendername="exploreHybridRecommend",composite_N=5,show_k=10,...){
+goRecommendation <- function(username,relevent_N=50,recommendername="exploreHybridRecommend",composite_N=5,show_k=10,controllername = "simpleHybridWeightControl",...){
   currentMission <- getCurrentMissionInfo(username = username)
   mission_id <- currentMission$mission_id
   mission_round <- currentMission$mission_round
